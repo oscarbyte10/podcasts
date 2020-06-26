9906d381c1d4dae6686e8d784a70c3568733fb84
oscarblanq <oscarblanq@gmail.com>
2020-06-26 23:15:31 +0200

    IndexPage refactorized

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage, GetServerSideProps } from "next";
-
+import { ChannelGrid } from "../components/ChannelGrid";
 import Link from "next/link";
 
 import { Layout } from "../components/Layout";
@@ -17,44 +17,10 @@ export const getServerSideProps: GetServerSideProps = async () => {
 const Index: NextPage<Props> = ({ channels }) => (
   <>
     <Layout title="Podcasts">
-      <section className="channels">
-        {channels.map((channel: any) => (
-          <Link href={`/channel?id=${channel.id}`} key={channel.id}>
-            <div className="channel">
-              <img src={channel.urls.logo_image.original} alt="" />
-              <h2>{channel.title}</h2>
-            </div>
-          </Link>
-        ))}
-      </section>
+      <ChannelGrid channels={channels} />
     </Layout>
 
-    <style jsx>
-      {`
-        .channels {
-          display: grid;
-          grid-gap: 15px;
-          padding: 15px;
-          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
-        }
-        .channel {
-          display: block;
-          border-radius: 3px;
-          box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
-          margin-bottom: 0.5em;
-        }
-        .channel img {
-          width: 100%;
-        }
-        h2 {
-          padding: 5px;
-          font-size: 0.9em;
-          font-weight: 600;
-          margin: 0;
-          text-align: center;
-        }
-      `}
-    </style>
+    <style jsx>{``}</style>
   </>
 );
 
