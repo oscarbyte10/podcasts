641af296435f32b2ac2cfe897bd46013b7e9b48d
oscarblanq <oscarblanq@gmail.com>
2020-06-28 01:41:24 +0200

    App finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import { NextPage, GetServerSideProps } from "next";
 import { ChannelGrid } from "../components/ChannelGrid";
-import Link from "next/link";
-
 import { Layout } from "../components/Layout";
 
 type Props = {
@@ -9,13 +7,18 @@ type Props = {
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const data = await fetch(`https://api.audioboom.com/channels/recommended`);
-  const { body: channels } = await data.json();
-  return { props: { channels } };
+  try {
+    const data = await fetch(`https://api.audioboom.com/channels/recommended`);
+    const { body: channels } = await data.json();
+    return { props: { channels } };
+  } catch (e) {
+    return { props: { channels: null } };
+  }
 };
 
 const Index: NextPage<Props> = ({ channels }) => (
   <>
+    {}
     <Layout title="Podcasts">
       <ChannelGrid channels={channels} />
     </Layout>
